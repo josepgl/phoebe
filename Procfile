f233e8ded8c0e3f8477fe5505b018fefcf0c13ac
Jose P. Garcia <josepgl@gmail.com>
2020-10-02 15:08:59 +0700

    Revert "chore: add download of javascript library on release"
    This reverts commit 1d4a8b583f1c95f2d447760699232acea1b33bda.

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: scripts/release.sh
+release: python phoebe/manage.py migrate
 web: gunicorn --chdir phoebe phoebe.wsgi --log-file -
--- scripts/release.sh
+++
@@ -1,5 +0,0 @@
-cd phoebe
-
-wget https://unpkg.com/lightweight-charts/dist/lightweight-charts.standalone.production.js -P static/js/
-
-python manage.py migrate
